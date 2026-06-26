0828fdb971336c249cb2fd6d3b44b0678d8d9072
Rafaz-7 <fezaguer@espol.edu.ec>
2026-06-26 18:06:09 -0500

    Esqueleto del proyecto

---
+++ TicketBeat-DisenoSoftware-G6/src/main/java/com/rafaelcosmo/ticketbeat/disenosoftware/g6/TicketBeatDisenoSoftwareG6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rafaelcosmo.ticketbeat.disenosoftware.g6;
+
+/**
+ *
+ * @author Rafael Cosmo
+ */
+public class TicketBeatDisenoSoftwareG6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
