d278e619e6db89fe22ef1cadda47488d1c8db188
colorboss1 <danegret@espol.edu.ec>
2026-06-26 19:17:32 -0500

    commit Daniel Negrete gestores

--- TicketBeat-DisenoSoftware-G6/src/main/java/com/ticketbeat/gestores/GestorIncidentes.java
+++ TicketBeat-DisenoSoftware-G6/src/main/java/com/ticketbeat/gestores/GestorIncidentes.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.ticketbeat.gestores;
 
-/**
- *
- * @author Rafael Cosmo
- */
+
+import com.ticketbeat.modelo.Incidente;
+import com.ticketbeat.modelo.Comprador;
+import com.ticketbeat.interfaces.IResolvedor;
+
 public class GestorIncidentes {
-    
+
+    public void reportarIncidente(String descripcion, IResolvedor agente, IResolvedor admin, Comprador comprador, GestorNotificaciones notificador) {
+        Incidente incidente = registrarYClasificar(descripcion);
+        
+        // Intento 1er nivel
+        boolean resuelto = agente.resolver(incidente);
+        
+        // Fragmento ALT del diagrama
+        if (!resuelto) {
+            System.out.println("El Agente NO puede resolver.");
+            admin.escalar(incidente);
+            resuelto = true; // El admin aplica resolución final
+        } else {
+            System.out.println("El Agente SI puede resolver.");
+        }
+
+        // Notificación final
+        if (resuelto) {
+            notificador.notificarResolucion(comprador, "Su incidente ha sido cerrado exitosamente.");
+        }
+    }
+
+    public Incidente registrarYClasificar(String descripcion) {
+        System.out.println("Incidente registrado y clasificado.");
+        return new Incidente(); // Retorna la entidad para ser procesada
+    }
 }
